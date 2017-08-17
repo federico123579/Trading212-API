25f06f14c83e4b0372dc3c1bc05bfed1cdf35ee2
Federico Lolli <federico123579@gmail.com>
2017-08-17 18:40:36 +0200

    Dev (#1)
    * HTTP requests added
    
    * Splinter chosen
    
    * Added logo
    
    * Added requirements
    
    * Updated README.md
    
    * Updated .gitignore
    
    * Started API developing
    
    * Added several new functions
    added:
    - login
    - logout
    - add movement
    
    * Updated .gitignore
    
    * Finished first-approach
    
    * Renamed api correctly

---
+++ lib/data.py
@@ -0,0 +1,18 @@
+path = {
+    'login-btn': "#login-button",
+    'log': "input.btn-head",
+    'logo': "div.nav_logo",
+    'alert-box': "span.weekend-trading-close",
+    'add-mov': "span.open-dialog-icon.svg-icon-holder",
+    'search-box': "div.searchbox input",
+    'first-res': "div.results.scrollable-holder > div",
+    'sell-btn': "div#orderdialog div.tradebox-button.tradebox-sell",
+    'buy-btn': "div#orderdialog div.tradebox-button.tradebox-buy",
+    'quantity': "div.quantity-slider-input-wrapper > input",
+    'limit-gain-unit': "input#uniqName_1_10",
+    'limit-gain-value': "input#uniqName_1_9",
+    'limit-loss-unit': "input#uniqName_1_14",
+    'limit-loss-value': "input#uniqName_1_13",
+    'confirm-btn': "div.orderdialog-confirm-button",
+    'data-table': "tbody.table-body.dataTable-show-currentprice-arrows",
+}
