9faed63320eda89a9e472ef4936550673116f304
Federico Lolli <federico123579@gmail.com>
2017-09-03 23:08:53 +0200

    Added test and fixed

--- tradingAPI/api.py
+++ tradingAPI/api.py
@@ -1,5 +1,3 @@
-#/usr/bin/env python3.6
-
 import re
 import selenium.common.exceptions
 from pyvirtualdisplay import Display
@@ -15,7 +13,7 @@ from .data import *
 class API(object):
     '''Interface object'''
 
-    def __init__(self, level):
+    def __init__(self, level="debug"):
         self.movements = []
         self.stocks = []
         self.logger = logger(level)
@@ -23,11 +21,33 @@ class API(object):
 
     def _css(self, css_path):
         '''css find function abbreviation'''
-        return self.browser.find_by_css(css_path)
+        fails = 0
+        exc = None
+        while fails < 3:
+            try:
+                result = self.browser.find_by_css(css_path)
+                return result
+            except Exception as e:
+                exc = e
+                fails += 1
+                sleep(0.5)
+        self.logger.error(e)
+        return 0
 
     def _name(self, name):
         '''name find function abbreviation'''
-        return self.browser.find_by_name(name)
+        fails = 0
+        exc = None
+        while fails < 3:
+            try:
+                result = self.browser.find_by_name(name)
+                return result
+            except Exception as e:
+                exc = e
+                fails += 1
+                sleep(0.5)
+        self.logger.error(e)
+        return 0
 
     def _elCss(self, css_path):
         '''check if element is present by css'''
@@ -38,32 +58,47 @@ class API(object):
         return re.findall(r"[-+]?\d*\.\d+|\d+", string)[0]
 
     def launch(self, brow="firefox"):
-        self.vbro.start()
-        self.logger.debug("virtual display launched")
-        self.browser = Browser(brow)
-        self.logger.debug("browser {brow} launched".format(brow=brow))
+        '''launch browser and virtual display'''
+        try:
+            self.vbro.start()
+            self.logger.debug("virtual display launched")
+        except Exception:
+            self.critical("virtual display failed to launch")
+            return 0
+        try:
+            self.browser = Browser(brow)
+            self.logger.debug("browser {brow} launched".format(brow=brow))
+        except Exception:
+            self.logger.critical("browser {brow} failed to launch".format(brow=brow))
+            return 0
+        return 1
 
     def login(self, username, password, mode="demo"):
         '''Login function'''
         url = "https://trading212.com/it/login"
-        self.logger.debug("visiting {url}".format(url=url))
         try:
             self.browser.visit(url)
+            self.logger.debug("visiting {url}".format(url=url))
         except selenium.common.exceptions.WebDriverException:
             self.logger.critical("connection timed out")
-        # self._css(path['login-btn']).click() obsolete
-        self._name("login[username]").fill(username)
-        self._name("login[password]").fill(password)
-        self._css(path['log']).click()
-        timeout = 30
-        while not self._elCss(path['logo']):
-            timeout -= 1
-            if timeout == 0:
-                self.logger.critical("login failed")
+            return 0
+        try:
+            self._name("login[username]").fill(username)
+            self._name("login[password]").fill(password)
+            self._css(path['log']).click()
+            timeout = 30
+            while not self._elCss(path['logo']):
+                timeout -= 1
+                if timeout == 0:
+                    self.logger.critical("login failed")
+                    return 0
             sleep(1)
-        self.logger.debug("logged in")
-        if mode == "demo" and self._elCss(path['alert-box']):
-            self._css(path['alert-box']).click()
+            self.logger.debug("logged in")
+            if mode == "demo" and self._elCss(path['alert-box']):
+                self._css(path['alert-box']).click()
+        except Exception:
+            self.logger.critical("login failed")
+            return 0
 
     def logout(self):
         '''logout func (to quit browser)'''
@@ -71,8 +106,10 @@ class API(object):
             self.browser.quit()
         except Exception:
             raise BrowserException("browser not started")
+            return 0
         self.vbro.stop()
         self.logger.debug("Logged out")
+        return 1
 
     def addMov(self, product, quantity=None, mode="buy", stop_limit=None):
         '''Add movement function'''
@@ -97,6 +134,7 @@ class API(object):
             {limit}".format(quant=bold(quantity), product=bold(product),
                             limit=bold(stop_limit)))
         sleep(1)
+        return 1
 
     def closeMov(self, mov_id):
         '''close a position'''
@@ -118,21 +156,25 @@ class API(object):
         movs = []
         count = 0
         for x in soup.find_all("tr"):
-            prod_id = x['id']
-            product = x.select("td.name")[0].text
-            quant = x.select("td.quantity")[0].text
-            if "direction-label-buy" in soup.find_all("tr")[0].select("td.direction")[0].span['class']:
-                mode = "long"
-            else:
-                mode = "short"
-            price = self._num(x.select("td.averagePrice")[0].text)
-            earn = self._num(x.select("td.ppl")[0].text)
-            mov = Movement(prod_id, product, quant, mode, price, earn)
-            movs.append(mov)
-            count += 1
+            try:
+                prod_id = x['id']
+                product = x.select("td.name")[0].text
+                quant = x.select("td.quantity")[0].text
+                if "direction-label-buy" in soup.find_all("tr")[0].select("td.direction")[0].span['class']:
+                    mode = "long"
+                else:
+                    mode = "short"
+                price = self._num(x.select("td.averagePrice")[0].text)
+                earn = self._num(x.select("td.ppl")[0].text)
+                mov = Movement(prod_id, product, quant, mode, price, earn)
+                movs.append(mov)
+                count += 1
+            except Exception as e:
+                self.logger.error(e)
         self.logger.debug(
             "{count} positions updated".format(count=bold(count)))
         self.movements = movs
+        return 1
 
     def checkStocks(self, stocks):
         '''check specified stocks (list)'''
@@ -156,38 +198,47 @@ class API(object):
                 stock.addVar([sell_price, sent])
                 count += 1
         self.logger.debug("added {count} stocks".format(count=bold(count)))
+        return 1
 
     def addPrefs(self, prefs):
         '''add prefered stocks'''
-        for pref in prefs:
-            self._css(path['search-btn'])[0].click()
-            self._css(path['all-tools'])[0].click()
-            self._css(path['search-pref'])[0].fill(pref)
-            if self._elCss(path['plus-icon']):
-                self._css(path['add-btn'])[0].click()
-        self._css(path['close-prefs'])[0].click()
-        self._css("span.prefs-icon-node")[0].click()
-        sleep(0.5)
-        self._css(
-            "div.item-tradebox-prefs-menu-list-sentiment_mode")[0].click()
-        self._css("span.prefs-icon-node")[0].click()
-        self.logger.debug("added {prefs} to preferencies".format(
-            prefs=', '.join([bold(x) for x in prefs])))
+        try:
+            for pref in prefs:
+                self._css(path['search-btn'])[0].click()
+                self._css(path['all-tools'])[0].click()
+                self._css(path['search-pref'])[0].fill(pref)
+                if self._elCss(path['plus-icon']):
+                    self._css(path['add-btn'])[0].click()
+            self._css(path['close-prefs'])[0].click()
+            self._css("span.prefs-icon-node")[0].click()
+            self._css(
+                "div.item-tradebox-prefs-menu-list-sentiment_mode")[0].click()
+            self._css("span.prefs-icon-node")[0].click()
+            self.logger.debug("added {prefs} to preferencies".format(
+                prefs=', '.join([bold(x) for x in prefs])))
+            return 1
+        except Exception as e:
+            self.logger.error(e)
+            return 0
 
     def clearPrefs(self):
         '''clear all stock preferencies'''
-        self._css(path['search-btn'])[0].click()
-        self._css(path['all-tools'])[0].click()
-        for res in self._css("div.search-results-list-item"):
-            if not res.find_by_css(path['plus-icon']):
-                res.find_by_css(path['add-btn'])[0].click()
-        sleep(0.5)
-        self._css('div.widget_message span.btn')[0].click()
-        # fix errors
-        self._css(path['close-prefs'])[0].click()
-        while not self._elCss(path['search-btn']):
-            time.sleep(0.5)
-        self.logger.debug("cleared preferencies")
+        try:
+            self._css(path['search-btn'])[0].click()
+            self._css(path['all-tools'])[0].click()
+            for res in self._css("div.search-results-list-item"):
+                if not res.find_by_css(path['plus-icon']):
+                    res.find_by_css(path['add-btn'])[0].click()
+            self._css('div.widget_message span.btn')[0].click()
+            # fix errors
+            self._css(path['close-prefs'])[0].click()
+            while not self._elCss(path['search-btn']):
+                time.sleep(0.5)
+            self.logger.debug("cleared preferencies")
+            return 1
+        except Exception as e:
+            self.logger.error(e)
+            return 0
 
 
 class Movement(object):
